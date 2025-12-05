52ad25b7588e2c2d49572f28d119c0ff634a47e3
Jose Carlos Velasco <jcvl0611@gmail.com>
2025-12-04 22:23:52 -0600

    Challenging bias

--- snippet.js
+++ snippet.js
@@ -1,7 +1,33 @@
-async function obtenerUsuario() {
-  const respuesta = await fetch("https://jsonplaceholder.typicode.com/users/1");
-  const data = await respuesta.json();
-  console.log(data);
+const https = require("https");
+
+function obtenerUsuario(callback) {
+  https.get("https://jsonplaceholder.typicode.com/users/1", (res) => {
+    let data = "";
+
+    // Se acumulan los chunks de datos
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    // Cuando la respuesta termina:
+    res.on("end", () => {
+      try {
+        const json = JSON.parse(data);
+        callback(null, json);
+      } catch (error) {
+        callback("Error al parsear JSON");
+      }
+    });
+  }).on("error", (err) => {
+    callback("Error de red: " + err.message);
+  });
 }
 
-obtenerUsuario();
+// Uso:
+obtenerUsuario((error, usuario) => {
+  if (error) {
+    console.log("Error:", error);
+  } else {
+    console.log("Usuario obtenido:", usuario);
+  }
+});
